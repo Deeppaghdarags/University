a8ceb0e9b2b08bab97ba7e95ca17cad5d10e114c
Deeppaghdar <paghdardeep74@gmail.com>
2022-03-24 10:52:50 +0530

    Source file original

---
+++ demo/src/main/java/com/test/demo/student.java
@@ -0,0 +1,22 @@
+package com.test.demo;
+import java.sql.*;
+
+public class student {
+	public static void main(String[] args) {
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/College","root","P@ssw0rd@123");
+			
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from student");
+			
+			while(rs.next()) {
+				System.out.println(rs.getString(1)+" "+rs.getInt(2)+" "+rs.getString(3));
+				
+			}
+			con.close();
+			
+		}catch(Exception e) {System.out.println(e);}
+		
+	}
+
+}
